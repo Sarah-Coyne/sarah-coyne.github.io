43647b12ead3c9b5d7ed9a823c4c3d1b4f14dc72
Megan Coyne <coynem6@udayton.edu>
2025-08-16 14:44:33 -0400

    fun stuff, projects, and interests

--- sarahcoyne/src/app/pages/fun/fun.component.ts
+++ sarahcoyne/src/app/pages/fun/fun.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavButtonsComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-fun',
-  imports: [NavButtonsComponent], 
+  standalone: true,
+  imports: [CommonModule, NavButtonsComponent],
   templateUrl: './fun.component.html',
-  styleUrl: './fun.component.css'
+  styleUrls: ['./fun.component.css'],
 })
 export class FunComponent {
-
+  photos = [
+    { src: 'assets/images/waffles.JPG', caption: 'Caption' },
+    { src: 'assets/images/sarah-headshot.jpeg', caption: "Caption" },
+    { src: 'assets/images/sarah-headshot.jpeg', caption: 'Caption' },
+    { src: 'assets/images/waffles.JPG', caption: 'Caption' },
+    { src: 'assets/images/waffles.JPG', caption: 'Caption' },
+    { src: 'assets/images/sarah-headshot.jpeg', caption: 'Caption' },
+    { src: 'assets/images/sarah-headshot.jpeg', caption: 'Caption' },
+    { src: 'assets/img/maine.JPG', caption: 'Caption' },
+    { src: 'assets/img/family.JPG', caption: 'Caption' },
+  ];
 }
