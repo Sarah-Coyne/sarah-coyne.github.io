da5ce4bc30ab11c137270fc76d7e419cdddc1563
Megan-Coyne <coynem6@udayton.edu>
2025-09-09 23:02:48 -0400

    fixes

--- sarahcoyne/src/app/pages/fun/fun.component.ts
+++ sarahcoyne/src/app/pages/fun/fun.component.ts
@@ -12,9 +12,9 @@ import { NavButtonsComponent } from '../nav-bar/nav-bar.component';
 export class FunComponent {
   photos = [
     { src: 'assets/images/running.jpg', caption: 'I have been a competitive distance runner for 12 years and my favorite event is the mile!' },
-    { src: 'assets/images/sisters.jpeg', caption: 'I have two sisters! Anna is my older sister and Megan is my twin, they helped me with this website. They are super awesome :)' },
+    { src: 'assets/images/Sisters.jpeg', caption: 'I have two sisters! Anna is my older sister and Megan is my twin, they helped me with this website. They are super awesome :)' },
     { src: 'assets/images/Miel.jpeg', caption: 'Food science people have the best coffee orders, my go to is a Cafe Miel!' },
-    { src: 'assets/images/Waffles.jpeg.jpeg', caption: 'I have a dog named Waffles, she is a huge diva and loves being outside!' },
+    { src: 'assets/images/Waffles.jpeg', caption: 'I have a dog named Waffles, she is a huge diva and loves being outside!' },
     { src: 'assets/images/Gameday1.jpeg', caption: 'I am a huge sports fan and love gamedays in East Lansing' },
     { src: 'assets/images/Tomatoes.JPG', caption: 'My family grows lots of beautiful tomatoes every year and they are so delicious!' },
     { src: 'assets/images/Sarah_hiking.jpeg', caption: 'I love hiking with my family. This summer we hiked the narrows in Zion!' },
